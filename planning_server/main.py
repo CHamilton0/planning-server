9743e537d0eb9a13669101bcea1baf375fd758a2
Christopher Hamilton <chrish001@live.com.au>
2024-03-17 16:34:27 +1030

    feat: add db connection

--- planning_server/main.py
+++ planning_server/main.py
@@ -2,7 +2,18 @@ import strawberry
 
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+from pymongo import MongoClient
 
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb://user:password@localhost:27017"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['database']
 
 @strawberry.type
 class Query:
@@ -15,5 +26,10 @@ schema = strawberry.Schema(Query)
 
 graphql_app = GraphQLRouter(schema)
 
+# Get the database
+dbname = get_database()
+
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
+
+dbname.test.insert_one({'x': 1})
